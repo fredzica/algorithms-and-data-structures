47a12e59d8bf3796bc44a069447ec97720cb1e87
Frederico Zica <fredzica@gmail.com>
2022-05-24 13:52:46 -0300

    applying TS linting only on TS files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,23 @@
 module.exports = {
   root: true,
-  parserOptions: {
-    project: ["tsconfig.json"],
-  },
   parser: "@typescript-eslint/parser",
+  env: {
+    node: true,
+  },
   plugins: ["@typescript-eslint", "jest"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+  extends: ["eslint:recommended"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+
+      parserOptions: {
+        project: ["./tsconfig.json"], // Specify it only for TypeScript files
+      },
+    },
   ],
 };
