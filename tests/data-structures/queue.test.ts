b4ca5d7ea8756d5772cb17a2a2fc26a72de356b9
Frederico Zica <fredzica@gmail.com>
2022-05-27 10:59:18 -0300

    testing the two queue implementations

--- tests/data-structures/queue.test.ts
+++ tests/data-structures/queue.test.ts
@@ -1,49 +1,65 @@
-import { Queue } from "../../src/data-structures/queue"
+import { LinkedListQueue, PureQueue, Queue } from "../../src/data-structures/queue"
 
 describe("Queue tests", () => {
+    const testImplementations = <T>(testFn: (q: Queue<T>) => void) => {
+        const types = [PureQueue, LinkedListQueue]
+        types.forEach((type) => {
+            const q = new type<T>()
+            testFn(q)
+        })
+    }
+
     it("Should add an element", () => {
-        const q = new Queue<string>()
+        const testAdd = (q: Queue<string>) => {
+            const added = "hello"
+            q.add(added)
+            expect(q.peek()).toEqual(added)
 
-        const added = "hello"
-        q.add(added)
-        expect(q.peek()).toEqual(added)
+            q.add("world")
+            expect(q.peek()).toEqual(added)
+        }
 
-        q.add("world")
-        expect(q.peek()).toEqual(added)
+        testImplementations(testAdd)
     })
 
     it("Should remove an element", () => {
-        const q = new Queue<string>()
+        const testRemove = (q: Queue<string>) => {
+            expect(q.remove()).toBeUndefined()
 
-        expect(q.remove()).toBeUndefined()
+            q.add("hello")
+            q.add("world")
 
-        q.add("hello")
-        q.add("world")
+            expect(q.remove()).toEqual("hello")
+            expect(q.remove()).toEqual("world")
+            expect(q.remove()).toBeUndefined()
+        }
 
-        expect(q.remove()).toEqual("hello")
-        expect(q.remove()).toEqual("world")
-        expect(q.remove()).toBeUndefined()
+        testImplementations(testRemove)
     })
 
     it("Should peek the first element", () => {
-        const q = new Queue<string>()
+        const testPeek = (q: Queue<string>) => {
+            expect(q.peek()).toBeUndefined()
 
-        expect(q.peek()).toBeUndefined()
+            const added = "hello"
+            q.add(added)
+            expect(q.peek()).toEqual(added)
 
-        const added = "hello"
-        q.add(added)
-        expect(q.peek()).toEqual(added)
+            q.add("world")
+            expect(q.peek()).toEqual(added)
+        }
 
-        q.add("world")
-        expect(q.peek()).toEqual(added)
+        testImplementations(testPeek)
     })
 
     it("Should correctly return if it's empty", () => {
-        const q = new Queue<number>()
+        const testIsEmpty = (q: Queue<number>) => {
+            expect(q.isEmpty()).toEqual(true)
 
-        expect(q.isEmpty()).toEqual(true)
+            q.add(33)
+            expect(q.isEmpty()).toEqual(false)
+        }
 
-        q.add(33)
-        expect(q.isEmpty()).toEqual(false)
+        testImplementations(testIsEmpty)
     })
 })
