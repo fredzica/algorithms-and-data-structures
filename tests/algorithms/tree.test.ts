9750bcb25f88c152d7b0326262d19d706ebd841d
Frederico Zica <fredzica@gmail.com>
2022-05-29 13:44:05 -0300

    actually testing in-order traversal

--- tests/algorithms/tree.test.ts
+++ tests/algorithms/tree.test.ts
@@ -3,14 +3,20 @@ import { BinaryTreeNode } from "../../src/data-structures/tree"
 import { inOrderTraversal } from "../../src/algorithms/tree"
 
 describe("Tree algorithms tests", () => {
+    const testTraversal = <T>(expectedElements: T[], node: BinaryTreeNode<T>) => {
+        console.log(node.element)
+        expect(expectedElements.shift()).toEqual(node.element)
+    }
+
     it("Should traverse a tree in order", () => {
-        const root = new BinaryTreeNode("0",
+        const root = new BinaryTreeNode(0,
             new BinaryTreeNode(
-                "1", new BinaryTreeNode("2"), new BinaryTreeNode("3")
+                1, new BinaryTreeNode(2), new BinaryTreeNode(3)
             ),
-            new BinaryTreeNode("4")
+            new BinaryTreeNode(4)
         )
 
-        inOrderTraversal(root, (node) => console.log(node.element))
+        const expectedElements = [2, 1, 3, 0, 4]
+        inOrderTraversal(root, (node) => testTraversal(expectedElements, node))
     })
 })
