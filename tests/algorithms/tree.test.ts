aa511b4db1d9638d0893cd2d1d2826c94a942649
Frederico Zica <fredzica@gmail.com>
2022-05-29 14:01:36 -0300

    better tree for tests

--- tests/algorithms/tree.test.ts
+++ tests/algorithms/tree.test.ts
@@ -1,6 +1,6 @@
 import { BinaryTreeNode } from "../../src/data-structures/tree"
 
-import { inOrderTraversal } from "../../src/algorithms/tree"
+import { inOrderTraversal, postOrderTraversal, preOrderTraversal } from "../../src/algorithms/tree"
 
 describe("Tree algorithms tests", () => {
     const testTraversal = <T>(expectedElements: T[], node: BinaryTreeNode<T>) => {
@@ -8,15 +8,36 @@ describe("Tree algorithms tests", () => {
         expect(expectedElements.shift()).toEqual(node.element)
     }
 
-    it("Should traverse a tree in order", () => {
-        const root = new BinaryTreeNode(0,
+    const root = new BinaryTreeNode(
+        0,
+        new BinaryTreeNode(
+            1,
+            new BinaryTreeNode(2),
             new BinaryTreeNode(
-                1, new BinaryTreeNode(2), new BinaryTreeNode(3)
-            ),
-            new BinaryTreeNode(4)
+                3,
+                new BinaryTreeNode(4),
+                new BinaryTreeNode(5),
+            )
+        ),
+        new BinaryTreeNode(
+            6,
+            undefined,
+            new BinaryTreeNode(7),
         )
+    )
 
-        const expectedElements = [2, 1, 3, 0, 4]
+    it("Should traverse a tree in order", () => {
+        const expectedElements = [2, 1, 4, 3, 5, 0, 6, 7]
         inOrderTraversal(root, (node) => testTraversal(expectedElements, node))
+    })
+
+    it("Should traverse a tree pre order", () => {
+        const expectedElements = [2, 1, 3, 0, 4]
+        preOrderTraversal(root, (node) => testTraversal(expectedElements, node))
+    })
+
+    it("Should traverse a tree post order", () => {
+        const expectedElements = [2, 1, 3, 0, 4]
+        postOrderTraversal(root, (node) => testTraversal(expectedElements, node))
     })
 })
