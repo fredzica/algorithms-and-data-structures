cf9d33877a4038226597e619ea6bc00160c1d21c
Frederico Zica <fredzica@gmail.com>
2022-06-01 16:51:01 -0300

    simplified the binary tree node code

--- src/data-structures/tree.ts
+++ src/data-structures/tree.ts
@@ -1,11 +1,8 @@
-class BinaryTreeNode<T> {
-    constructor(
-        public element: T,
-        public left?: BinaryTreeNode<T>,
-        public right?: BinaryTreeNode<T>
-    ) { }
+interface BinaryTreeNode<T> {
+    element: T,
+    left?: BinaryTreeNode<T>,
+    right?: BinaryTreeNode<T>
 }
-
 
 const inOrderTraversal = <T>(node: BinaryTreeNode<T>, visit: (node: BinaryTreeNode<T>) => void) => {
     if (node.left) {
--- tests/data-structures/tree.test.ts
+++ tests/data-structures/tree.test.ts
@@ -5,36 +5,38 @@ describe("Tree algorithms tests", () => {
         expect(expectedElements.shift()).toEqual(node.element)
     }
 
-    const root = new BinaryTreeNode(
-        0,
-        new BinaryTreeNode(
-            1,
-            new BinaryTreeNode(2),
-            new BinaryTreeNode(
-                3,
-                new BinaryTreeNode(4),
-                new BinaryTreeNode(5),
-            )
-        ),
-        new BinaryTreeNode(
-            6,
-            undefined,
-            new BinaryTreeNode(7),
-        )
-    )
+    const traversalTestsRoot: BinaryTreeNode<number> = {
+        element: 0,
+        left: {
+            element: 1,
+            left: { element: 2 },
+            right: {
+                element: 3,
+                left: { element: 4 },
+                right: { element: 5 },
+            }
+        },
+        right: {
+            element: 6,
+            right: { element: 7 },
+        }
+    }
 
     it("Should traverse a tree in order", () => {
         const expectedElements = [2, 1, 4, 3, 5, 0, 6, 7]
-        inOrderTraversal(root, (node) => testTraversal(expectedElements, node))
+        inOrderTraversal(traversalTestsRoot, (node) => testTraversal(expectedElements, node))
     })
 
     it("Should traverse a tree pre order", () => {
         const expectedElements = [0, 1, 2, 3, 4, 5, 6, 7]
-        preOrderTraversal(root, (node) => testTraversal(expectedElements, node))
+        preOrderTraversal(traversalTestsRoot, (node) => testTraversal(expectedElements, node))
     })
 
     it("Should traverse a tree post order", () => {
         const expectedElements = [2, 4, 5, 3, 1, 7, 6, 0]
-        postOrderTraversal(root, (node) => testTraversal(expectedElements, node))
+        postOrderTraversal(traversalTestsRoot, (node) => testTraversal(expectedElements, node))
+    })
+
+    it("Should check if a tree is full", () => {
     })
 })
