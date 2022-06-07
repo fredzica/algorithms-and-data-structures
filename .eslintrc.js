028704741e1b240c333800388e3ac3ecc6b9faee
Frederico Zica <fredzica@gmail.com>
2022-06-07 14:35:57 -0300

    attempt to correctly configure prettier with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,15 @@ module.exports = {
   env: {
     node: true,
   },
-  plugins: ["@typescript-eslint", "jest"],
-  extends: ["eslint:recommended"],
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"], // Your TypeScript files extension
-
-      // As mentioned in the comments, you should extend TypeScript plugins here,
-      // instead of extending them outside the `overrides`.
-      // If you don't want to extend any rules, you don't need an `extends` attribute.
-      extends: ["plugin:@typescript-eslint/recommended", "prettier"],
-
-      parserOptions: {
-        project: ["./tsconfig.json"], // Specify it only for TypeScript files
-      },
-    },
+  plugins: ["@typescript-eslint", "prettier", "jest"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
+  rules: {
+    "no-console": 1, // Means warning
+    "prettier/prettier": 2, // Means error
+  },
 };
