b1fbf3d555354d539f36116649a85928d86e45d6
Frederico Zica <fredzica@gmail.com>
2022-09-16 16:36:37 -0300

    sorting algorithms tests

---
+++ src/algorithms/sort.ts
@@ -0,0 +1,37 @@
+/**
+ * Sorts an array in place with the insertion sort algorithm.
+ * @param array The array to sort
+ * @returns The sorted array
+ */
+const insertionSort = <T>(array: T[]): T[] => {
+  return array
+}
+
+/**
+ * Sorts an array in place with the selection sort algorithm.
+ * @param array The array to sort
+ * @returns The sorted array
+ */
+const selectionSort = <T>(array: T[]): T[] => {
+  return array
+}
+
+/**
+ * Sorts an array in place with the mergesort algorithm.
+ * @param array The array to sort
+ * @returns The sorted array
+ */
+const mergeSort = <T>(array: T[]): T[] => {
+  return array
+}
+
+/**
+ * Sorts an array in place with the quicksort algorithm.
+ * @param array The array to sort
+ * @returns The sorted array
+ */
+const quickSort = <T>(array: T[]): T[] => {
+  return array
+}
+
+export { insertionSort, selectionSort, mergeSort, quickSort }
---
+++ test/algorithms/sort.test.ts
@@ -0,0 +1,28 @@
+import {
+  insertionSort,
+  mergeSort,
+  quickSort,
+  selectionSort,
+} from '../../src/algorithms/sort'
+
+describe('Sort algorithms', () => {
+  it('should sort arrays of numbers', () => {
+    const testSort = <T>(array: T[], expected: T[]) => {
+      ;[insertionSort, selectionSort, mergeSort, quickSort].forEach((sortFn) =>
+        expect(sortFn(array)).toEqual(expected),
+      )
+    }
+
+    testSort([], [])
+    testSort([10], [10])
+
+    testSort([7, 6, 5, 4, 3, 2, 1], [1, 2, 3, 4, 5, 6, 7])
+    testSort([1, 2, 3, 4, 5, 6, 7], [1, 2, 3, 4, 5, 6, 7])
+
+    const bigArray = [...Array(100).keys()]
+    const shuffledBigArray = [...Array(100).keys()].sort(
+      () => Math.random() - 0.5,
+    )
+    testSort(shuffledBigArray, bigArray)
+  })
+})
