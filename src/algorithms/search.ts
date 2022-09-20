0e93ff21d3c3f24213b50414e077bdf289fcb5c6
Frederico Zica <fredzica@gmail.com>
2022-09-20 18:34:13 -0300

    binary search tests

---
+++ src/algorithms/search.ts
@@ -0,0 +1,5 @@
+const binarySearch = (arr: number[], target: number): number => {
+  return -1
+}
+
+export { binarySearch }
---
+++ test/algorithms/search.test.ts
@@ -0,0 +1,20 @@
+import { binarySearch } from '../../src/algorithms/search'
+
+describe('Search algorithms', () => {
+  it('Should do binary search on an empty array', () => {
+    expect(binarySearch([], 0)).toBe(-1)
+  })
+
+  it('Should do binary search on an array with a single element', () => {
+    expect(binarySearch([1], 1)).toBe(1)
+    expect(binarySearch([1], 2)).toBe(-1)
+  })
+
+  it('Should do binary search on a long array', () => {
+    const array = Array.from([...Array(100).keys()])
+    expect(binarySearch(array, 1)).toBe(1)
+    expect(binarySearch(array, 43)).toBe(43)
+    expect(binarySearch(array, 89)).toBe(89)
+    expect(binarySearch(array, 112)).toBe(-1)
+  })
+})
